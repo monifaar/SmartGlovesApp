1cb0a57151adc60178043209fa81fb4b9a19967a
Monifa Arini <monifacool78@gmail.com>
2021-08-01 05:54:16 +0700

    modified class (actvity login for adding intent), home and login xml, and themes

--- SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/LoginActivity.kt
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/LoginActivity.kt
@@ -1,13 +1,29 @@
 package com.garda.smartglovesapp1.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.garda.smartglovesapp1.R
+import com.garda.smartglovesapp1.home.HomeActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnMoveActivity: Button = findViewById(R.id.button_signin)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button_signin -> {
+                val moveIntent = Intent(this, HomeActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 
 }
