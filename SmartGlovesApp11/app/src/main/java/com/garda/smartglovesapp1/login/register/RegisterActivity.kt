889bdb77f663a07ef6310a4380f402d5b65f5d23
Monifa Arini <monifacool78@gmail.com>
2021-08-03 06:18:33 +0700

    new file register class and xml

---
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.garda.smartglovesapp1.login.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.garda.smartglovesapp1.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
