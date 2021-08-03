268622dc4d2b7028917e91c861db5f37d0c0fa0f
Monifa Arini <monifacool78@gmail.com>
2021-08-03 15:50:11 +0700

    Update LoginActivity.kt

--- SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/LoginActivity.kt
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.garda.smartglovesapp1.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,56 +10,59 @@ import android.widget.Toast
 import com.garda.smartglovesapp1.R
 import com.garda.smartglovesapp1.home.HomeActivity
 import com.garda.smartglovesapp1.login.db.LoginHelper
+import com.garda.smartglovesapp1.login.db.LoginModel
 import com.garda.smartglovesapp1.login.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity(){
     lateinit var edtEmail: EditText
     lateinit var edtPassword: EditText
-    lateinit var db:LoginHelper
     lateinit var btnMoveActivity: Button
-    lateinit var text:TextView
+    lateinit var textNoAcc:TextView
+    lateinit var loginHelper: LoginHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         btnMoveActivity = findViewById(R.id.button_signin)
+        btnMoveActivity.setOnClickListener{login()}
 
-        text = findViewById(R.id.btn_reg)
-        text.setOnClickListener{
+        textNoAcc = findViewById(R.id.btn_reg)
+        textNoAcc.setOnClickListener{
             val moveIntent = Intent(this, RegisterActivity::class.java)
             startActivity(moveIntent)
         }
 
         edtEmail = findViewById(R.id.email)
         edtPassword = findViewById(R.id.password)
-        db = LoginHelper(this)
+        loginHelper = LoginHelper(this)
 
-//        btnMoveActivity.setOnClickListener(object : View.OnClickListener{
-//            override fun onClick(v: View?) {
-//                val email:String = edtEmail.text.toString()
-//                val pass: String = edtPassword.text.toString()
-//                val check = db.checkEmailPass(email, pass)
-//        when (check == true) {
-//            check-> {
-//                Toast.makeText(this@LoginActivity, "Sign In Success",Toast.LENGTH_SHORT).show()
-//                    val moveIntent = Intent(this@LoginActivity, HomeActivity::class.java)
-//                    startActivity(moveIntent)
-//                }
-//            else -> Toast.makeText(this@LoginActivity, "Sign In Failed",Toast.LENGTH_SHORT).show()
-//            }
-//
-//            }
-//
-//        })
     }
-//    override fun onClick(v: View?) {
-//        when (v?.id) {
-//                R.id.btn_reg -> {
-//                    val moveIntent = Intent(this, RegisterActivity::class.java)
-//                    startActivity(moveIntent)
-//                }
-//            }
-//
-//        }
+
+    fun login() {
+        val email:String = edtEmail.text.toString()
+        val pass:String = edtPassword.text.toString()
+
+        if (email.isNotEmpty() && pass.isNotEmpty() )  {
+            if(true){
+                try {
+                    val login = loginHelper.checkEmailPass(email = email, password = pass)
+                    if (login) {
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+                catch (e:Exception) {
+                    e.printStackTrace()
+                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(this, "Password is not same", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else {
+            Toast.makeText(this, "Field is not required", Toast.LENGTH_SHORT).show()
+        }
     }
+}
