86f9e202f76298e81c9669d0654de70a9059b643
Monifa Arini <monifacool78@gmail.com>
2021-08-03 15:49:56 +0700

    Update LoginHelper.kt

--- SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/db/LoginHelper.kt
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/db/LoginHelper.kt
@@ -1,9 +1,6 @@
 package com.garda.smartglovesapp1.login.db
 
-import android.content.ContentValues
 import android.content.Context
-import android.database.SQLException
-import android.database.sqlite.SQLiteDatabase
 
 class LoginHelper(context: Context) {
 
@@ -11,7 +8,7 @@ class LoginHelper(context: Context) {
 
     fun checkEmailPass(email:String, password:String):Boolean {
         val db = dataBaseHelper.writableDatabase
-        val query = "select * from DatabaseContract.FeedLogin.TABLE_NAME where email = $email and password = $password"
+        val query = "select * from id where email = $email and password = $password"
         val cursor = db.rawQuery(query,null)
         if (cursor.count<=0) {
             cursor.close()
@@ -24,15 +21,5 @@ class LoginHelper(context: Context) {
     }
 }
 
-//    fun checkEmail(email:String):Boolean {
-//        val db = dataBaseHelper.writableDatabase
-//        val cursor = db.rawQuery("SELECT * FROM" + DatabaseContract.FeedLogin.TABLE_NAME + "WHERE" + DatabaseContract.FeedLogin.COLUMN_NAME_EMAIL + "='" + email + "'", null)
-//        if (cursor.count>0) {
-//            return true
-//        }
-//        else {
-//            return false
-//        }
-//    }
 
 
