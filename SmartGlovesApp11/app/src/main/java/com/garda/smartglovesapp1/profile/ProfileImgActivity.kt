83364142234ed18130e03f388c1b63e2b4611e81
Monifa Arini <monifacool78@gmail.com>
2021-08-04 20:18:41 +0700

    Create ProfileImgActivity.kt

---
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/profile/ProfileImgActivity.kt
@@ -0,0 +1,12 @@
+package com.garda.smartglovesapp1.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.garda.smartglovesapp1.R
+
+class ProfileImgActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_img)
+    }
+}
