51733057f1c9a59d6ebe8b51a6cc822f52a403f2
Monifa Arini <monifacool78@gmail.com>
2021-08-04 10:57:42 +0700

    update

--- SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/LoginActivity.kt
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.garda.smartglovesapp1.login
 
 import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
