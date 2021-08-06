ec5233320310a8c2f9e38e0b13095c27427a46ce
Monifa Arini <monifacool78@gmail.com>
2021-08-06 18:31:46 +0700

    Update ProfileImgActivity.kt

--- SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/profile/ProfileImgActivity.kt
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/profile/ProfileImgActivity.kt
@@ -5,6 +5,15 @@ import android.os.Bundle
 import com.garda.smartglovesapp1.R
 
 class ProfileImgActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_TYPE_FORM = "extra_type_form"
+        const val EXTRA_RESULT = "extra_result"
+        const val RESULT_CODE = 101
+        const val TYPE_ADD = 1
+        const val TYPE_EDIT = 2
+        private const val FIELD_REQUIRED = "Field tidak boleh kosong"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_img)
