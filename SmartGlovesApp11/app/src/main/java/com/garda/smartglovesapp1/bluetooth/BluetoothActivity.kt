b98f4cb51741fe5e63f13fec001363b2240ba35f
Monifa Arini <monifacool78@gmail.com>
2021-08-15 16:24:41 +0700

    Create BluetoothActivity.kt

---
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/bluetooth/BluetoothActivity.kt
@@ -0,0 +1,16 @@
+package com.garda.smartglovesapp1.bluetooth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.garda.smartglovesapp1.R
+
+
+class BluetoothActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+
+        }
+
+}
