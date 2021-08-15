c8e98e78968f5a83e82651098ac1c08bace83c92
Monifa Arini <monifacool78@gmail.com>
2021-08-15 16:24:34 +0700

    Update HomeFragment.kt

--- SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/home/HomeFragment.kt
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/home/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.garda.smartglovesapp1.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import com.garda.smartglovesapp1.R
+import com.garda.smartglovesapp1.bluetooth.BluetoothActivity
+import com.garda.smartglovesapp1.profile.ProfileImgActivity
 
 class HomeFragment : Fragment() {
 
@@ -15,6 +19,16 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val img_btn:ImageButton = view.findViewById(R.id.butut_btn)
+        img_btn.setOnClickListener {
+            requireActivity().run{
+                startActivity(Intent(this, BluetoothActivity::class.java))
+                finish()
+            }
+        }
+    }
     companion object {
         fun newInstance(): HomeFragment{
             val fragment = HomeFragment()
