1911d198b972c319bdc309e207b2eb4caf5740b7
Monifa Arini <monifacool78@gmail.com>
2021-08-22 14:54:42 +0700

    BluetoothActivity.kt update

--- SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/bluetooth/BluetoothActivity.kt
+++ SmartGlovesApp11/app/src/main/java/com/garda/smartglovesapp1/bluetooth/BluetoothActivity.kt
@@ -50,8 +50,30 @@ class BluetoothActivity : AppCompatActivity(){
 
         }
 
-        val mOffBtn2:Button = findViewById(R.id.offBtn)
-        mOffBtn2.setOnClickListener {
+        mOnBtn.setOnClickListener(object : OnClickListener() {
+            fun onClick(v: View?) {
+                if (!mBlueAdapter.isEnabled()) {
+                    showToast("Turning On Bluetooth...")
+                    //intent to on bluetooth
+                    val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    startActivityForResult(intent, REQUEST_ENABLE_BT)
+                } else {
+                    showToast("Bluetooth is already on")
+                }
+            }
+        })
+        //discover bluetooth btn click
+        mDiscoverBtn.setOnClickListener(object : OnClickListener() {
+            fun onClick(v: View?) {
+                if (!mBlueAdapter.isDiscovering()) {
+                    showToast("Making Your Device Discoverable")
+                    val intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+                    startActivityForResult(intent, REQUEST_DISCOVER_BT)
+                }
+            }
+        })
+        //off btn click
+        mOffBtn.setOnClickListener(object : OnClickListener() {
             fun onClick(v: View?) {
                 if (mBlueAdapter.isEnabled()) {
                     mBlueAdapter.disable()
@@ -61,7 +83,46 @@ class BluetoothActivity : AppCompatActivity(){
                     showToast("Bluetooth is already off")
                 }
             }
+        })
+        //get paired devices btn click
+        mPairedBtn.setOnClickListener(object : OnClickListener() {
+            fun onClick(v: View?) {
+                if (mBlueAdapter.isEnabled()) {
+                    mPairedTv.setText("Paired Devices")
+                    val devices = mBlueAdapter.getBondedDevices()
+                    for (device in devices) {
+                        mPairedTv.append(
+                                """
+                                
+                                Device: ${device.name}, $device
+                                """.trimIndent()
+                        )
+                    }
+                } else {
+                    //bluetooth is off so can't get paired devices
+                    showToast("Turn on bluetooth to get paired devices")
+                }
+            }
+        })
         }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_ENABLE_BT -> if (resultCode == RESULT_OK) {
+                //bluetooth is on
+                mBlueIv.setImageResource(R.drawable.ic_action_on)
+                showToast("Bluetooth is on")
+            } else {
+                //user denied to turn bluetooth on
+                showToast("could't on bluetooth")
+            }
         }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    //toast message function
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
 
 }
